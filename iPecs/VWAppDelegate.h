51f228165e3227d08ddfa106854d3f4b770cc736
Julio Vidal <juliocvidal@Julios-MacBook-Pro.local>
2014-02-04 01:22:32 -0200

    Initial Commit

---
+++ iPecs/VWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VWAppDelegate.h
+//  iPecs
+//
+//  Created by Julio Vidal on 2/4/14.
+//  Copyright (c) 2014 vw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iPecsTests/iPecsTests.m
@@ -0,0 +1,34 @@
+//
+//  iPecsTests.m
+//  iPecsTests
+//
+//  Created by Julio Vidal on 2/4/14.
+//  Copyright (c) 2014 vw. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iPecsTests : XCTestCase
+
+@end
+
+@implementation iPecsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
